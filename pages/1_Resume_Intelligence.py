4ee715d64ea5f3d4a5a64b72915d666e48e2744d
Thomas Mohanraj <thomasmohanraj2012@gmail.com>
2026-08-27 09:29:02 +0100

    Checkpoint before multipage conversion

---
+++ pages/1_Resume_Intelligence.py
@@ -0,0 +1,30 @@
+# import streamlit as st
+
+# st.title("🚨 THIS IS THE RESUME PAGE")
+# st.success("Multipage navigation is working")
+
+# st.set_page_config(
+#     page_title="Resume Intelligence",
+#     page_icon="📄",
+#     layout="wide"
+# )
+
+# st.title("📄 Resume Intelligence")
+
+# st.success(
+#     "Resume Intelligence Page Loaded Successfully"
+# )
+
+import streamlit as st
+
+st.set_page_config(
+    page_title="TalentCopilotAI",
+    page_icon="🤖",
+    layout="wide"
+)
+
+st.title("🤖 TalentCopilotAI")
+
+st.write(
+    "Home Page"
+)
